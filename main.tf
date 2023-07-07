f1e45ba9bf81da4b2d3d4db3514e6673db8143fa
Olalken Famoroti <lekanfamoroti@gmail.com>
2023-07-07 12:35:08 -0500

    create security groups

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example_instance" {
   ami                    = var.AMI # Replace with the desired AMI ID
   instance_type          = var.instance_type
   key_name               = "mykey"
-  availability_zone      = var.availability_zone 
+  availability_zone      = var.availability_zone
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
   subnet_id              = aws_subnet.subnet.id
 
