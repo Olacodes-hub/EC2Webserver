ea09b741c5708d5f522cf8b096c65067f7980f13
Olalken Famoroti <lekanfamoroti@gmail.com>
2023-07-07 12:32:29 -0500

    create a vpc

---
+++ vpc.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-az1"
+  }
+}
+
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "dev-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "route-table"
+  }
+}
+
+resource "aws_route_table_association" "subnet_route_table" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route-table.id
+}
